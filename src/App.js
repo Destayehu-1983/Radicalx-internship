a012e7558c99da371ca30ca6e3f84c95d56263cd
Destayehu-1983 <104433663+Destayehu-1983@users.noreply.github.com>
2022-12-13 12:43:20 -0500

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,4 @@
 
-// import Header from "./Components/Header";
-// import Middlepage from "./Components/Middlepage";
-// import Navbar from "./Components/Navbar";
-// function App() {
-//   return (
-//     <div className="App">
-//    <Navbar />
-//    <Header />
-//    <Middlepage />
-//     </div>
-//   );
-// }
-
-// export default App;
 import React from "react";
 import Header from "./Components/Header";
 import Middlepage from "./Components/Middlepage";
@@ -29,22 +15,6 @@ import { height } from "@mui/system";
   
 const App = () => {
   
-// Sample data
-const data = [
-  { argument: 'monday', value: 30 },
-  { argument: 'Tuesday', value: 20 },
-  { argument: 'Wednesday', value: 10 },
-  { argument: 'Thursday', value: 50 },
-  { argument: 'Friday', value: 60 },
-  {argument:'saturday', value:70 },
-  {argument:'saturday', value:70 },
-  {argument:'sunday', value:40 },
-  {argument:'day1', value:44 },
-  
-
-
-
-];
 return (
 
     <>
